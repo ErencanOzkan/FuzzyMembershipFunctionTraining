e94bce255b29591e25b5a34345cb35e59b6cf771
erencan <erencan@netas.com.tr>
2016-07-14 19:55:46 +0300

    first commit

---
+++ src/main/java/com/fuzzy/stocks/FuzzyMembershipFunctionTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.fuzzy.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzyMembershipFunctionTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzyMembershipFunctionTrainingApplication.class, args);
+	}
+}
